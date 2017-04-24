67e6cfaec4f4bcd412f7c0bb5db3c472225fae0b
zzjoh <zz.john.cn@gmail.com>
2017-04-24 09:53:23 +0800

    add requirements.txt

--- source/conf.py
+++ source/conf.py
@@ -84,7 +84,7 @@ exclude_patterns = ['Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 pygments_style = 'sphinx'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
-todo_include_todos = False
+todo_include_todos = True
 
 
 # -- Options for HTML output ----------------------------------------------
